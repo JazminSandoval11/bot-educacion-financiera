4f726b4057cabb02ec7c19867ee297ef32efc6d1
JazminSandoval11 <jazmin.sandoval93@uabc.edu.mx>
2025-04-02 13:30:45 -0700

    Update bot_credito.py

--- bot_credito.py
+++ bot_credito.py
@@ -428,7 +428,7 @@ def procesar_mensaje(mensaje, numero):
                 estado_usuario.pop(numero)
                 return (
                     f"💸 Si pagaras este crédito sin hacer abonos extra, terminarías pagando ${float(total_sin):,.2f} en total.\n"
-                    f"Pero si decides abonar ${float(contexto['abono']):,.2f} adicionales por periodo desde el periodo {inicio_abono}...\n"
+                    f"Pero si decides abonar ${float(contexto['abono']):,.2f} adicionales por periodo desde el periodo {desde}...\n"
                     f"✅ Terminarías de pagar en menos tiempo (¡te ahorras {pagos_menos} pagos!)\n"
                     f"💰 Pagarías ${float(total_con):,.2f} en total\n"
                     f"🧮 Y te ahorrarías ${float(ahorro):,.2f} solo en intereses.\n\n"
